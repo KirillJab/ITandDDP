ac8dc60300fb3a13db1889f655c523b29760cd43
Kirill Yablonsky <blackshark564@gmail.com>
2022-05-19 15:18:46 +0300

    Change tags styles and margins.

--- AliasTheGame/src/components/header.js
+++ AliasTheGame/src/components/header.js
@@ -1,8 +1,8 @@
 export const Header = () => `
 <div class="menu-header-background"></div>
 <div class="menu-header">
-    <button class="go-back-button" id="go-back-btn">
+    <a class="button go-back-button" id="go-back-btn">
         <span class="material-icon">arrow_back</span>
-    </button>
+    </a>
     <span class="game-name">Alias</span>
 </div>`;
--- AliasTheGame/src/components/teamsSettings.js
+++ AliasTheGame/src/components/teamsSettings.js
@@ -7,11 +7,11 @@ import "../styles/last-word.css";
 export const TeamsSettings = ({ isLastWordForAll, teams }) => `
 ${Header()}
 <section class="last-word-section">
-    <aside>
-        <span class="material-icon">campaign_two_tone</span>
+    <aside class="row-aside">
+        <span class="aside-icon material-icon">campaign_two_tone</span>
     </aside>
-    <article>
-        <h3>Последнее слово для всех</h3>
+    <article class="row-article">
+        <h2>Последнее слово для всех</h2>
         <p>слово, неотгаданное в лимит времени, могут угадывать все</p>
     </article>
     <label class="checkbox">
@@ -23,7 +23,7 @@ ${Header()}
 </section>
 
 <section class="teams-section">
-    <h2>Команды</h2>
+    <h1>Команды</h1>
     <div class="bold-underline"></div>
     <ul>
     ${teams
@@ -39,7 +39,7 @@ ${Header()}
               <input type="text" placeholder="Название команды" value="${
                 team.name
               }" id="edit-team-name-${index}" maxlength="16">
-              <button id="delete-team-${index}">
+              <button class="button" id="delete-team-${index}">
                   <span class="material-icon">close</span>
               </button>
           </div>
@@ -52,11 +52,11 @@ ${Header()}
         
     </ul>
     <div class="controls">
-        <button class="button-naked add-team-button" id="add-team">
+        <button class="button button-naked add-team-button" id="add-team">
             Добавить команду
         </button>
     </div>
 </section>
-<button class="goback-button" id="prev-settings-btn">Назад</button>
-<button class="next-button" id="next-btn">Начать</button>
+<a class="button goback-button" id="prev-settings-btn">Назад</a>
+<a class="button next-button" id="next-btn">Начать</a>
 </div>`;
